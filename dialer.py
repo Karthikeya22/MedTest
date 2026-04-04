02e04f136e0d7a4b8c63bce76758481b1fbab8c0
Karthikeya22 <imkarthik44@gmail.com>
2026-04-04 18:44:55 -0400

    dialer file updates

--- dialer.py
+++ dialer.py
@@ -1,15 +1,11 @@
 import os
 from urllib.parse import urlencode
-from dotenv import load_dotenv
-
-load_dotenv()
-
 from twilio.rest import Client
 
-TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
-TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
-TWILIO_NUMBER = os.environ.get("TWILIO_NUMBER")
-BASE_URL = os.environ.get("BASE_URL")  # e.g. https://your-service.onrender.com
+TWILIO_ACCOUNT_SID = os.environ["TWILIO_ACCOUNT_SID"]
+TWILIO_AUTH_TOKEN = os.environ["TWILIO_AUTH_TOKEN"]
+TWILIO_NUMBER = os.environ["TWILIO_NUMBER"]
+BASE_URL = os.environ["BASE_URL"]
 
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
@@ -18,7 +14,6 @@ def call_parent(row):
     row is a dict-like object with:
     - parent_phone
     - patient_name
-    - parent_name (optional)
     - visit_label
     - insurance_type
     """
